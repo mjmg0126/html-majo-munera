8890987545c5da69d982afc251d3b0aaf33cc44c
mjmg0126 <123569720+mjmg0126@users.noreply.github.com>
2023-02-08 10:33:00 -0500

    08/02

---
+++ app.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "Hulk",
+      img: "images/Hulk.jpg"
+    },
+    {
+      name: "batman",
+      img: "images/batman.jpg"
+    },
+    {
+      name: "flash",
+      img: "images/flash.jpg"
+    },
+    {
+      name: "ironman",
+      img: "images/ironman.jpg"
+    },
+    {
+      name: "spiderman",
+      img: "images/spiderman.jpg"
+    },
+    {
+      name: "supergirl",
+      img: "images/supergirl.jpg"
+    },
+    {
+      name: "superman",
+      img: "images/superman.jpg"
+    },
+    {
+      name: "wonderwoman",
+      img: "images/wonderwoman.jpg"
+    }
+  ];
+});
